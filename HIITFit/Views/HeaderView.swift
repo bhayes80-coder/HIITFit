8b2369bcf5fb1e65c0d42db9920dbd05a6f97207
Barry Hayes <bhayes16@live.spcollege.edu>
2025-08-31 03:25:38 -0400

    ch3-a

--- HIITFit/Views/HeaderView.swift
+++ HIITFit/Views/HeaderView.swift
@@ -32,24 +32,23 @@
 
 import SwiftUI
 
-struct ExerciseView: View {
-    let videoNames = ["squat", "step-up", "burpee", "sun-salute"]
-    let exerciseNames = ["Squat", "Step Up", "Burpee", "Sun Salute"]
-    let index: Int
-    
+struct HeaderView: View {
+    let exerciseName: String
     var body: some View {
         VStack {
-            HeaderView(exerciseName: exerciseNames[index])
-            Text("Video player")
-            Text("Timer")
-            Text("Start/Done button")
-            Text("Rating")
-            Text("History button")
-        }    }
+            Text(exerciseName)
+                .font(.largeTitle)
+            HStack {
+                Image(systemName: "1.circle")
+                Image(systemName: "2.circle")
+                Image(systemName: "3.circle")
+                Image(systemName: "4.circle")
+            }
+            .font(.title2)
+        }
+    }
 }
 
-#Preview {
-    ExerciseView(index: 0)
+#Preview (traits: .sizeThatFitsLayout) {
+    HeaderView(exerciseName: "Squat")
 }
-
-
